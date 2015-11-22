95a5bb9f0a8c9b3440ce928bb829104f8d975af1
Aravind Balla <bsaaravind@gmail.com>
2015-11-22 12:52:11 +0530

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -11,15 +11,33 @@ ERROR CASES: Return '\0' for invalid inputs.
 NOTES: Don't create new string.
 */
 
-char removeSpaces(char *str) {
-	int i, j = 0;
-	char *noSpaces;
+void RemovePart(char *str, int a, int b)
+{
+	for (; str[a] != '\0', str[b] != '\0'; a++, b++)
+	{
+		str[a] = str[b];
+	}
+}
 
-	if(str = "") return '\0';
+char removeSpaces(char *str) {
+	int i, j;
+
+	if(str == "" || !str) return '\0';
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ')
-			noSpaces[j++] = str[i];
+		if (str[i] == ' ')
+		{
+			j = 0;
+			while (str[i + j + 1] == ' ') j++;
+			RemovePart(str, i, i+j+1);
+		}
 	}
+
+	i = 0;
+	while ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'z')) i++;
+	for (; str[i] != '\0'; i++)
+		str[i] = '\0';
+
+	return '\0';
 }
